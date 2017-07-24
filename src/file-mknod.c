7ea93fff6d9e0cc2d3aec1c94e41babf79f85f39
Uchio KONDO <udzura@pepabo.com>
2017-07-24 14:58:56 +0900

    Initial implementation

--- src/file-mknod.c
+++ src/file-mknod.c
@@ -1,71 +1,71 @@
 /*
-** mrb_file.c - File class
+** file-mknod.c - File class with mknod method
 **
 ** Copyright (c) Uchio Kondo 2017
 **
 ** See Copyright Notice in LICENSE
 */
 
-#include "mruby.h"
-#include "mruby/data.h"
+#define _BSD_SOURCE
+
+#include <mruby.h>
+#include <mruby/data.h>
+#include <mruby/error.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "mrb_file.h"
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-typedef struct {
-  char *str;
-  int len;
-} mrb_file_data;
+#define mrb_mknod_symbol_p(type, expected) (type == mrb_intern_lit(mrb, expected))
 
-static const struct mrb_data_type mrb_file_data_type = {
-  "mrb_file_data", mrb_free,
-};
-
-static mrb_value mrb_file_init(mrb_state *mrb, mrb_value self)
+static mrb_value mrb_file_mknod(mrb_state *mrb, mrb_value self)
 {
-  mrb_file_data *data;
-  char *str;
-  int len;
+  char *path;
+  mode_t mode = 0644;
+  int major, minor;
+  dev_t dev;
+  mrb_sym type;
+  mrb_get_args(mrb, "nzii|i", &type, &path, &major, &minor, &mode);
 
-  data = (mrb_file_data *)DATA_PTR(self);
-  if (data) {
-    mrb_free(mrb, data);
+  dev = makedev(major, minor);
+
+  if (mrb_mknod_symbol_p(type, "block")) {
+    mode = S_IFBLK | mode;
+  } else if (mrb_mknod_symbol_p(type, "char")) {
+    mode = S_IFCHR | mode;
+  } else if (mrb_mknod_symbol_p(type, "regular")) {
+    mode = S_IFREG | mode;
+  } else {
+    mrb_raise(mrb, E_ARGUMENT_ERROR, "Invalid device type");
   }
-  DATA_TYPE(self) = &mrb_file_data_type;
-  DATA_PTR(self) = NULL;
 
-  mrb_get_args(mrb, "s", &str, &len);
-  data = (mrb_file_data *)mrb_malloc(mrb, sizeof(mrb_file_data));
-  data->str = str;
-  data->len = len;
-  DATA_PTR(self) = data;
+  if (mknod(path, mode, dev) < 0) {
+    mrb_sys_fail(mrb, "mknod failed");
+  }
 
-  return self;
-}
-
-static mrb_value mrb_file_hello(mrb_state *mrb, mrb_value self)
-{
-  mrb_file_data *data = DATA_PTR(self);
-
-  return mrb_str_new(mrb, data->str, data->len);
-}
-
-static mrb_value mrb_file_hi(mrb_state *mrb, mrb_value self)
-{
-  return mrb_str_new_cstr(mrb, "hi!!");
+  return mrb_true_value();
 }
 
 void mrb_mruby_file_mknod_gem_init(mrb_state *mrb)
 {
-    struct RClass *file;
+  struct RClass *file;
+  if (mrb_class_defined(mrb, "File")) {
+    file = mrb_class_get(mrb, "File");
+  } else {
     file = mrb_define_class(mrb, "File", mrb->object_class);
-    mrb_define_method(mrb, file, "initialize", mrb_file_init, MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, file, "hello", mrb_file_hello, MRB_ARGS_NONE());
-    mrb_define_class_method(mrb, file, "hi", mrb_file_hi, MRB_ARGS_NONE());
-    DONE;
+  }
+
+  mrb_define_class_method(mrb, file, "mknod", mrb_file_mknod, MRB_ARGS_ARG(4, 1));
+  mrb_define_class_method(mrb, file, "makedev", mrb_file_mknod, MRB_ARGS_ARG(4, 1));
+
+  DONE;
 }
 
 void mrb_mruby_file_mknod_gem_final(mrb_state *mrb)
 {
 }
-
